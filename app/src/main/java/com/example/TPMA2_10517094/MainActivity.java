7a5dcd3fad0b34ac47ca098906e9436f4aba2a93
mohamadalvir <mohamadalvin@gmail.com>
2020-08-09 00:38:15 +0700

    Floating button

--- app/src/main/java/com/example/TPMA2_10517094/MainActivity.java
+++ app/src/main/java/com/example/TPMA2_10517094/MainActivity.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -78,23 +76,9 @@ public class MainActivity extends AppCompatActivity {
             }});
         ((ArrayAdapter)ListView01.getAdapter()).notifyDataSetInvalidated();
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_read) {
-            Intent iread = new Intent(this, MainActivity.class);
-            startActivity(iread);
-            return true;
-        } else if (id == R.id.action_create) {
-            Intent icreate = new Intent(this, CreateTripActivity.class);
-            startActivity(icreate);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+
+    public void intentCreate(View view){
+        Intent icreate = new Intent(this, CreateTripActivity.class);
+        startActivity(icreate);
     }
 }
