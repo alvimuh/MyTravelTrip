08a1045e262a760144deaa14a2aef1f73deffaff
mohamadalvir <mohamadalvin@gmail.com>
2020-08-11 12:33:02 +0700

    Add edit button

--- app/src/main/java/com/example/TPMA2_10517094/DetailTripActivity.java
+++ app/src/main/java/com/example/TPMA2_10517094/DetailTripActivity.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -36,24 +37,10 @@ public class DetailTripActivity extends AppCompatActivity {
             text4.setText(cursor.getString(4).toString());
         }
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_read) {
-            Intent iread = new Intent(this, MainActivity.class);
-            startActivity(iread);
-            return true;
-        } else if (id == R.id.action_create) {
-            Intent icreate = new Intent(this, CreateTripActivity.class);
-            startActivity(icreate);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void intentUpdate(View view){
+        String selection = getIntent().getStringExtra(DataHelper.MyColumns.id_trip);
+        Intent u = new Intent(getApplicationContext(),UpdateTripActivity.class);
+        u.putExtra(DataHelper.MyColumns.id_trip, selection);
+        startActivity(u);
     }
 }
